790dcae8c22615a9aa4aa89169c1f8fe77bb2c8e
Zzeongyx2 <39085743+Zzeongyx2@users.noreply.github.com>
2020-09-17 21:19:03 +0900

    commit check

--- src/kr/ac/jbnu/se/awp/gitplay4/HelloGit.java
+++ src/kr/ac/jbnu/se/awp/gitplay4/HelloGit.java
@@ -3,5 +3,7 @@ package kr.ac.jbnu.se.awp.gitplay4;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!!!!_ eclipase");
+
+		System.out.println("Hello Git 4");
 	}
 }
